f56a38b53850a6c1e74864673e82b97ce232ee90
CooperStansbury <cstansbu@umich.edu>
2018-11-09 12:11:47 -0500

    build classifier for research vs consent forms

--- tools/form_classifier.py
+++ tools/form_classifier.py
@@ -2,15 +2,69 @@
 
 import pandas as pd
 import argparse
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.model_selection import cross_val_score
 
 
 def trainModels(src):
 
     df = pd.read_csv(src)
 
-    print(df.dtypes)
-
+    # print(df.dtypes)
     # print(df.head(10))
+    # print(df.columns)
+
+    columns_to_drop = []
+
+    # convert categorical fields to codes
+    for field in df.columns:
+        if df[field].dtype == 'object':
+            columns_to_drop.append(field)
+
+    df = df.drop(columns_to_drop, axis=1)
+    labels = df['research_consent']
+    df = df.drop(['research_consent'], axis=1)
+
+    for column in df.columns:
+        if df[column].isnull().values.any():
+            df[column].fillna(0, inplace=True)
+
+    # correlations
+    corr_matrix = df.corr()
+    print(corr_matrix)
+    print()
+
+    models = []
+
+    models.append(("LogisticRegression",LogisticRegression()))
+    models.append(("SVC",SVC()))
+    models.append(("LinearSVC",LinearSVC()))
+    models.append(("KNeighbors",KNeighborsClassifier()))
+    models.append(("DecisionTree",DecisionTreeClassifier()))
+    models.append(("RandomForest",RandomForestClassifier()))
+    rf2 = RandomForestClassifier(n_estimators=100, criterion='gini',
+                                    max_depth=10, random_state=0, max_features=None)
+    models.append(("RandomForest2",rf2))
+    models.append(("MLPClassifier",MLPClassifier(solver='lbfgs', random_state=0)))
+
+    results = []
+    names = []
+    for name, model in models:
+        result = cross_val_score(model, df, labels,  cv=3)
+        names.append(name)
+        results.append(result)
+
+    for i in range(len(names)):
+        print(names[i],results[i].mean())
+
+
 
 
 if __name__ == "__main__":
